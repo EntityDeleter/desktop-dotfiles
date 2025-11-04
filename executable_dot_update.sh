44d773d6f2002d533fb98d24f53b748eea5779c9
EntityDeleter <uiop2028@gmail.com>
2025-11-04 20:31:31 +0530

    Update .update.sh

--- executable_dot_update.sh
+++ executable_dot_update.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Update Repositories Recursively:
-sudo pacman -Syu --config ~/.pacmanrepofiles/core.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/core-testing.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/extra.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/extra-testing.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/multilib.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/multilib-testing.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/cachyos-v4.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/cachyos-core-v4.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/cachyos-extra-v4.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/cachyos.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/endeavouros.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/core.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/core-testing.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/extra.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/extra-testing.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/multilib.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/multilib-testing.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/cachyos-v4.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/cachyos-core-v4.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/cachyos-extra-v4.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/cachyos.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/endeavouros.conf --noconfirm |& grep -v "is newer than"
 
 # Update:
 topgrade
 arch-update -d -s
 
 # Cleanup:
-sudo rm -rf ~/.cache/* /tmp/*
+sudo-rs rm -rf ~/.cache/* /tmp/*
 conda clean --all
 #nix-store --gc | rg -vi "deleting "
 
 # Update Package Lists:
-~/.diff.sh
+pacman -Qqe > /etc/pkglist.txt
