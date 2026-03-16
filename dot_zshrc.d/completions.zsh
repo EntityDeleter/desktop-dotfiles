e2e78551db33f8a1f1a6cbf9236faf1b04351272
EntityDeleter <uiop2028@gmail.com>
2026-03-16 14:30:24 +0530

    Update .zshrc.d/completions.zsh

--- dot_zshrc.d/completions.zsh
+++ dot_zshrc.d/completions.zsh
@@ -1,4 +1,4 @@
 eval "$(zoxide init zsh)"
 eval "$(thefuck --alias oops)"
 eval "$(mcat --generate zsh)"
-eval "$(llama-cli --completion-bash)"
+eval "$(llama-cli --completion-bash | grep -vi 'ggml_cuda_init: found 1 CUDA devices (Total VRAM: 8098 MiB):' | grep -vi 'Device 0: NVIDIA GeForce GTX 1080, compute capability 6.1, VMM: yes, VRAM: 8098 MiB (7455 MiB free)')"
