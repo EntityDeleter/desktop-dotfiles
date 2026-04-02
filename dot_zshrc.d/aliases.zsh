90324df2a0059c94a8f9df725d932cc2e2aa23e2
EntityDeleter <uiop2028@gmail.com>
2026-04-02 17:39:36 +0530

    Update .zshrc.d/aliases.zsh

--- dot_zshrc.d/aliases.zsh
+++ dot_zshrc.d/aliases.zsh
@@ -14,8 +14,8 @@ alias openssh="ssh -p 39901 "
 alias tailssh="ssh "
 alias mosh="mosh --ssh='ssh -p 39901' "
 
-alias cat="mcat -t vscode -S --report --opts 'center=true,inline=false' --hyprlink -a "
-alias icat="mcat -t vscode -S --report --opts 'center=true,inline=false' -a --hyprlink -o interactive "
+alias cat="mcat -t vscode -S --report --hyprlink -a "
+alias icat="mcat -t vscode -S --report --hyprlink -a -o interactive "
 
 cl() {
   z "$1"
